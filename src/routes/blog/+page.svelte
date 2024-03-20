91d3b2c0852379953a9e5d1eeaa29b621d8d482d
Zahar <kikhtenko06@gmail.com>
2024-03-21 01:58:44 +0700

    kym

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -8,15 +8,16 @@
              <h2>{page.title}</h2>
 <p>{page.body}</p>
 <p class='readmore'>
-<a class="link" href={'/blog/posts/${page.id}'}>
+<a class="link" href={'./blog/posts/${page.id}'}>
 Читать далее
 </a>
 </p>
 </div>
 {/each}
 </div>
+
 <script>
-    import {routes} from './routes'
+    import {routes} from './routes.js'
 </script>
 
 
