a3d0744cbd6fc355c6727370c97fc6e2135d39ab
Zahar <kikhtenko06@gmail.com>
2024-03-20 23:16:10 +0700

    svetle change

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+
+<nav>
+<div class="nav-container">
+<a href="/" class="nav-logo" title="Back to Homepage">Кихтенко Захар</а> 
+    <div class="nav-links">
+<a href="/about" class="link">Обо мнe</a>
+ <a href="/blog" class="link">Блoг</a>
+<a href="/projects" class="link">Портфолио</a>
+ <a href="/contact" class="link">Kонтaкты</a>
+</div>
+</div>
+ </nav>
+<div class="container">
+<!-- Pages will be injected below -->
+<slot></slot>
+</div>
+<style>
+.container {
+max-width: 1400px;
+margin: 50px auto;
+}
+</style>
